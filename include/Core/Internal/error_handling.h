7b7f4f149573d0a0ef878378b133fb2634771bec
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-09-13 18:13:05 +0200

    cleaner error ids

--- include/Core/Internal/error_handling.h
+++ include/Core/Internal/error_handling.h
@@ -1,6 +1,12 @@
 #ifndef ERROR_HANDLING_H
 #define ERROR_HANDLING_H
 
+#define ERR_GLFW_INIT 1
+#define ERR_CREATE_OGL_WINDOW 2
+#define ERR_GLEW_INIT 3
+#define ERR_SHADER_COMPILE 4
+#define ERR_SHADER_LINK 5
+
 namespace logger {
 	void throw_critical_error(int err_id, const char* message);
 	void throw_warning(int err_id, const char* message);
